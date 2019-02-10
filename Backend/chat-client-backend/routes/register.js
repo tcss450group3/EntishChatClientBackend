b7e7385fb19282a1f4775e8a6540745dfa6f78d1
BuildTools <unconfigured@null.spigotmc.org>
2019-02-10 10:20:12 -0800

    Update register.js

--- Backend/chat-client-backend/routes/register.js
+++ Backend/chat-client-backend/routes/register.js
@@ -38,7 +38,8 @@ router.post('/', (req, res) => {
             res.send({
                 success: false,
                 input: req.body,
-                error: "The email address is invalid."
+                error: "The email address is invalid.",
+                field: "email"
             });
         }
 
@@ -46,7 +47,8 @@ router.post('/', (req, res) => {
             res.send({
                 success: false,
                 input: req.body,
-                error: "The password should be at least 6 digits."
+                error: "The password should be at least 6 digits.",
+                field: "password"
             });
         }
         
