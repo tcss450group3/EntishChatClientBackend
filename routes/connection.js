4363010810c35cd84970fdc0784c43efff4bdb42
BuildTools <unconfigured@null.spigotmc.org>
2019-02-24 12:40:59 -0800

    Update connection.js
    added ability to add and verify connections

--- routes/connection.js
+++ routes/connection.js
@@ -10,6 +10,82 @@ router.use(bodyParser.json());
 //send a message to all users "in" the chat session with chatId
 
 //Get all of the messages from a chat session with id chatid
+
+router.post("/new", (req, res) => {
+
+    let email = req.body['email'];
+    let username = req.body['username'];
+    let MemberID_A = req.body['id'];
+    
+    if(username == 0){
+        db.manyOrNone('SELECT MemberID FROM MEMBERS WHERE Email = $1', [email])
+        .then((data) => {
+            let MemberID_B = data.body['MemberID'];
+            db.none('INSERT INTO Connections (MemberID_A, MemberID_B, Verified) VALUES ($1, $2, 1)', [MemberID_A], [MemberID_B]) 
+            .then(() => {
+                res.send({
+                    success: true
+                })
+            }).catch((error) => {
+                console.log(error);
+                res.send({
+                    success: false,
+                    error: error
+                })
+            });
+        }).catch((error) => {
+            console.log(error);
+            res.send({
+                success: false,
+                error: error
+            })
+        });
+    } else {
+        db.manyOrNone('SELECT MemberID FROM MEMBERS WHERE Email = $1', [username])
+        .then((data) => {
+            let MemberID_B = data.body['MemberID'];
+            db.none('INSERT INTO Connections (MemberID_A, MemberID_B, Verified) VALUES ($1, $2, 1)', [MemberID_A], [MemberID_B]) 
+            .then(() => {
+                res.send({
+                    success: true
+                })
+            }).catch((error) => {
+                console.log(error);
+                res.send({
+                    success: false,
+                    error: error
+                })
+            });
+        }).catch((error) => {
+            console.log(error);
+            res.send({
+                success: false,
+                error: error
+            })
+        });
+    }
+      
+ });
+
+ router.post("/accept", (req, res) => {
+
+    let connectionID = req.body['id'];
+    
+    db.manyOrNone('UPDATE Connections SET Verification = -1 WHERE PrimaryKey = $1', [connectionID])
+    .then(() => {
+        res.send({
+            success: true
+        })
+    }).catch((error) => {
+        console.log(error);
+        res.send({
+            success: false,
+            error: error
+        })
+    });
+ });
+
+
 router.post("/", (req, res) => {
     let id = req.body['id'];
     db.manyOrNone('SELECT Username, verified FROM MEMBERS, CONNECTIONS WHERE MemberID = MemberID_A AND MemberID_B = $1 UNION SELECT Username, Verified FROM MEMBERS, CONNECTIONS WHERE MemberID = MemberID_B AND MemberID_A = $1 ', [id])
@@ -21,7 +97,7 @@ router.post("/", (req, res) => {
     }).catch((error) => {
         console.log(error);
         res.send({
-success: false,
+            success: false,
             error: error
         })
 }); });
